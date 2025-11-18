4895fd6ee7ac47bab2da16d6278164d899a1e83f
diitrii <lakedre21@gmail.com>
2025-11-18 10:51:01 -0800

    First commit

---
+++ createnewplayer.py
@@ -0,0 +1,8 @@
+class Player:
+    def __init__(player, name):
+        player.name = name
+        player.unbankedPoints = 0
+        player.bankedPoints = 0
+
+    def __repr__(player):
+        return f"{player.name}: unbanked={player.unbankedPoints}, banked={player.bankedPoints}"
