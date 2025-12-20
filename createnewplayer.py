fc8febde61361f37742b69060c94031320fbf857
diitrii <lakedre21@gmail.com>
2025-12-19 16:58:24 -0800

    Formatting changes

--- createnewplayer.py
+++ createnewplayer.py
@@ -1,8 +1,7 @@
 class Player:
     def __init__(player, name):
         player.name = name
-        player.unbankedPoints = 0
-        player.bankedPoints = 0
+        player.Points = 0
 
     def __repr__(player):
-        return f"{player.name}: unbanked: {player.unbankedPoints}, banked: {player.bankedPoints}"
+        return f"{player.name} points: {player.Points}"
